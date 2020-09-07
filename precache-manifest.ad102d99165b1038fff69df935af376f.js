924b36b8d54f93da35a6b0f127ef7d5d1138e9ac
bds0900 <Dbeak9336@conestogac.on.ca>
2020-09-06 23:15:07 -0400

    Updates

---
+++ precache-manifest.ad102d99165b1038fff69df935af376f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cca3d9cefa36e527c77b494ff1b615",
+    "url": "/shoppie-front/index.html"
+  },
+  {
+    "revision": "2ccc3e5259c9f3a33df3",
+    "url": "/shoppie-front/static/css/main.ef1c1500.chunk.css"
+  },
+  {
+    "revision": "c68d46c32b2f27589b2b",
+    "url": "/shoppie-front/static/js/2.0225b9e5.chunk.js"
+  },
+  {
+    "revision": "0fce063a4ec3cb005e7265693b407036",
+    "url": "/shoppie-front/static/js/2.0225b9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccc3e5259c9f3a33df3",
+    "url": "/shoppie-front/static/js/main.91332208.chunk.js"
+  },
+  {
+    "revision": "d10024877b1adfa0f418",
+    "url": "/shoppie-front/static/js/runtime-main.7ef4b3e5.js"
+  }
+]);
